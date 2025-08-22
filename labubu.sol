79598862a7b0ce560811499b8036d0d15a38301b
Drew Roberts <github@drewroberts.com>
2025-08-21 20:54:46 -0400

    Change uniswap router too mainnet eth

--- labubu.sol
+++ labubu.sol
@@ -1203,7 +1203,7 @@ contract LabubuDoge is ERC20, Ownable {
 
     constructor() ERC20("Labubu Doge", "LABUBU") {
         uniswapV2Router = IUniswapV2Router02(
-            0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24
+            0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
         );
         _approve(address(this), address(uniswapV2Router), type(uint256).max);
 
